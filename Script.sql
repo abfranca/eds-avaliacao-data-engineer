7b8b4d83efebbaf74ea07c8f2f39589671679a79
Felipe França <alexandre.b.franca@gmail.com>
2023-07-29 17:17:56 -0300

    entrega final

---
+++ Script.sql
@@ -0,0 +1,144 @@
+--0. Adicionei alguns scripts para ajudar no momento de validar as soluções.
+create schema if not exists stg_hospital_a;
+
+create schema if not exists stg_hospital_b;
+
+create schema if not exists stg_hospital_c;
+
+create schema if not exists stg_prontuario;
+
+create table if not exists stg_hospital_a.paciente (
+id serial PRIMARY KEY,
+nome varchar(255) not null,
+dt_nascimento date not null,
+cpf varchar(11) unique not null,
+nome_mae varchar(255) not null,
+dt_atualizacao timestamp not null
+);
+
+create table if not exists stg_hospital_b.paciente (
+id serial PRIMARY KEY,
+nome varchar(255) not null,
+dt_nascimento date not null,
+cpf varchar(11) unique not null,
+nome_mae varchar(255) not null,
+dt_atualizacao timestamp not null
+);
+
+create table if not exists stg_hospital_c.paciente (
+id serial PRIMARY KEY,
+nome varchar(255) not null,
+dt_nascimento date not null,
+cpf varchar(11) unique not null,
+nome_mae varchar(255) not null,
+dt_atualizacao timestamp not null
+);
+
+insert into stg_hospital_a.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+values (
+'paciente01', '1999-01-01', '12345678901', 'mae01', '2023-01-01 10:10:00'
+);
+
+insert into stg_hospital_a.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+values (
+'paciente02', '1999-01-01', '12345678902', 'mae02', '2023-01-01 10:11:00'
+);
+
+insert into stg_hospital_a.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+values (
+'paciente03', '1999-01-01', '12345678903', 'mae03', '2023-01-01 10:12:00'
+);
+
+insert into stg_hospital_b.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+values (
+'paciente01', '1999-01-01', '12345678901', 'mae01', '2023-01-01 10:12:00'
+);
+
+insert into stg_hospital_b.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+values (
+'paciente02', '1999-01-01', '12345678902', 'mae02', '2023-01-01 10:13:00'
+);
+
+insert into stg_hospital_c.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+values (
+'paciente01', '1999-01-01', '12345678901', 'mae01', '2023-01-01 10:11:00'
+);
+
+insert into stg_hospital_c.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+values (
+'paciente02', '1999-01-01', '12345678902', 'mae02', '2023-01-01 10:12:00'
+);
+
+--1. Modifiquei o tipo de cpf para um tipo textual,
+--   adicionei a constraint not null para todos os atributos.
+create table if not exists stg_prontuario.paciente (
+id serial PRIMARY KEY,
+nome varchar(255) not null,
+dt_nascimento date not null,
+cpf varchar(11) not null,
+nome_mae varchar(255) not null,
+dt_atualizacao timestamp not null
+);
+
+--2. 
+insert into stg_prontuario.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+select p.nome, p.dt_nascimento, p.cpf, p.nome_mae, p.dt_atualizacao
+from stg_hospital_a.paciente p;
+
+insert into stg_prontuario.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+select p.nome, p.dt_nascimento, p.cpf, p.nome_mae, p.dt_atualizacao
+from stg_hospital_b.paciente p;
+
+insert into stg_prontuario.paciente (
+nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
+)
+select p.nome, p.dt_nascimento, p.cpf, p.nome_mae, p.dt_atualizacao
+from stg_hospital_c.paciente p;
+
+--3.
+select * from stg_prontuario.paciente p
+where (select count(pp.cpf) from stg_prontuario.paciente pp
+where p.cpf like pp.cpf) > 1
+order by p.cpf;
+
+--4.
+select * from stg_prontuario.paciente p
+where (select count(pp.cpf) from stg_prontuario.paciente pp
+where p.cpf like pp.cpf) > 1 and
+(select max(pp.dt_atualizacao) from stg_prontuario.paciente pp
+where p.cpf like pp.cpf) = p.dt_atualizacao;
+
+--7. Criaria uma tabela stg_prontuario.atendimento_medico e uma tabela stg_prontuario.diagnostico
+--   que teria uma coluna id_atendimento que guardaria uma chave estrangeira com uma referência
+--   para uma linha da tabela stg_prontuario.atendimento_medico.
+
+--8.
+--   select (
+--   select count(*)
+--   from stg_prontuario.diagnostico d
+--   where d.id_atendimento in (
+--   select am.id
+--   from stg_prontuario.atendimento_medico am
+--   where am.tipo like 'U')) / (
+--   select count(*)
+--   from stg_prontuario.atendimento_medico am
+--   where am.tipo like 'U')
+--   as media_diagnosticos_urgencia;
---
+++ script.py
@@ -0,0 +1,93 @@
+# 5.
+import psycopg2
+
+# O seguinte import seria para realizar requisições HTTP e assim conseguir consumir apis
+# import requests
+
+import datetime
+
+import pandas
+
+connection = psycopg2.connect(
+    database="postgres",
+    user="postgres",
+    password="4dmin@%",
+    host="127.0.0.1",
+    port="5432",
+)
+
+connection.autocommit = True
+
+cursor = connection.cursor()
+
+sql1 = """create table if not exists stg_prontuario.copia_sigtap(\
+id serial PRIMARY KEY,\
+procedimento varchar(50) not null,\
+medicamento varchar(50) not null,\
+opm varchar(50) not null);"""
+
+# cursor.execute(sql1)
+
+sql2 = """copy stg_prontuario.copia_sigtap(\
+procedimento, medicamento, opm)\
+from 'C:\git\eds-avaliacao-data-engineer\data.csv'\
+delimiter ','\
+csv header;"""
+
+# cursor.execute(sql2)
+
+# 6.
+# As próximas duas linhas exemplificam o consumo de uma api em um determinado endpoint
+# api_url = "https://sigtap.api/registros"
+# response = requests.get(api_url).json()
+
+# Essa seria a consulta do problema 5 adaptada para o uso de objetos do tipo JSON
+# sql3 = """insert into stg_prontuario.copia_sigtap\
+# select procedimento, medicamento, opm\
+# from json_populate_recordset(null::stg_prontuario.copia_sigtap, %s);"""
+
+# E essa seria a forma de executar o script sql passando o objeto salvo na variável response
+# cursor.execute(sql3, response)
+
+
+# 9.
+def is_prescicao_possivel(prescricao, estoque):
+    is_possivel = True
+    aux = prescricao
+    while is_possivel and aux:
+        ch = aux[0]
+        if estoque.count(ch) < aux.count(ch):
+            is_possivel = False
+        aux = aux.replace(ch, "")
+    return is_possivel
+
+
+print(is_prescicao_possivel("a", "b"))
+
+print(is_prescicao_possivel("aa", "b"))
+
+print(is_prescicao_possivel("aa", "aab"))
+
+print(is_prescicao_possivel("aba", "cbaa"))
+
+# 10.
+datas = [
+    datetime.datetime(2020, 5, 17),
+    datetime.datetime(2020, 5, 18),
+    datetime.datetime(2020, 5, 18),
+    datetime.datetime(2020, 5, 17),
+    datetime.datetime(2020, 5, 17),
+    datetime.datetime(2020, 5, 19),
+]
+
+datas_df = pandas.DataFrame(list(map(lambda x: x.strftime("%m/%d/%Y"), datas)))
+
+datas_grafico = datas_df.value_counts().plot(kind="bar")
+
+datas_grafico.set_xlabel("Data")
+
+datas_grafico.set_ylabel("Atendimentos")
+
+connection.commit()
+
+connection.close()
